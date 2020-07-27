bef89b3bef5a93d0670e43b5e78d1f50de02cd3e
Srinibas Biswal <srinibasbiswal12@gmail.com>
2020-07-27 23:47:18 +0530

    Add Top Panel Container structure

---
+++ src/components/TopPanelComponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import classes from '*.module.css';
+import classes from '../stylesheets/style.module.css';
+
+function TopPanelComponent(props) {
+    return (
+        <div className={classes.AppHeader}>
+
+        </div>
+        <div className={classes.TopPanelStatic}>
+
+        </div>
+    );
+}
+
+export default TopPanelComponent;
