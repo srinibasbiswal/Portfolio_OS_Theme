6248f717560e9ab57a0e4c44e76c6be8e394b809
Srinibas Biswal <srinibasbiswal12@gmail.com>
2020-08-01 13:12:55 +0530

    Add AppExplorer Icons

--- src/components/AppModal.js
+++ src/components/AppModal.js
@@ -1,28 +1,61 @@
 import React from 'react';
-import { Modal, Button } from 'react-bootstrap';
+import styles from '../stylesheets/style.module.css';
+import { Modal, Button, Figure } from 'react-bootstrap';
+import CloseIcon from '../assets/images/CloseIcon.png';
+import MaximizeIcon from '../assets/images/MaximizeIcon.png';
+import MinimizeIcon from '../assets/images/MinimizeIcon.png';
 
-function AppModal(props){
-    return(
-        <Modal
-        {...props}
-        size="lg"
-        backdrop="static"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-          {props.data}
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <h4> {props.data}</h4>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={props.onHide}>Close</Button>
-        </Modal.Footer>
-      </Modal>
-    );
+function AppModal(props) {
+	return (
+		<Modal
+			{...props}
+			size="lg"
+			backdrop="static"
+			aria-labelledby="contained-modal-title-vcenter"
+			centered
+		>
+			<div className={`${styles.modalStaticTopBar}`}>
+				<div className={`d-flex flex-row-reverse bd-highlight`}>
+					<Figure className={`bd-highlight`} onClick={props.onHide}>
+						<Figure.Image
+							width={25}
+							height={25}
+							alt="CloseIcon"
+							src={CloseIcon}
+						/>
+					</Figure>
+					<Figure className={`bd-highlight`}>
+						<Figure.Image
+							width={25}
+							height={25}
+							alt="MaximizeIcon"
+							src={MaximizeIcon}
+						/>
+					</Figure>
+					<Figure className={`bd-highlight`}>
+						<Figure.Image
+							width={25}
+							height={25}
+							alt="MinimizeIcon"
+							src={MinimizeIcon}
+						/>
+					</Figure>
+				</div>
+			</div>
+			<Modal.Header>
+
+				<Modal.Title id="contained-modal-title-vcenter">
+					{props.data}
+				</Modal.Title>
+			</Modal.Header>
+			<Modal.Body>
+				<h4> {props.data}</h4>
+			</Modal.Body>
+			<Modal.Footer>
+				<Button onClick={props.onHide}>Close</Button>
+			</Modal.Footer>
+		</Modal>
+	);
 }
 
 export default AppModal;
--- src/containers/DesktopIconContainer.js
+++ src/containers/DesktopIconContainer.js
@@ -4,23 +4,23 @@ import DocumentsIcon from '../assets/images/DocumentsIcon.png';
 import DesktopIconComponent from '../components/DesktopIconComponent';
 import AppModal from '../components/AppModal';
 
-function DesktopIconContainer(){
+function DesktopIconContainer() {
 
     const [desktopIcons, setdesktopIcons] = useState({
         icons: [
-            { 
-                name: 'This Person', 
-                iconName: BatmanEmojiIcon, 
+            {
+                name: 'This Person',
+                iconName: BatmanEmojiIcon,
                 id: 1,
                 modalId: 'thisPersonModal',
-                modalShow : false
+                modalShow: false
             },
-            { 
-                name: 'Documents', 
-                iconName: DocumentsIcon, 
+            {
+                name: 'Documents',
+                iconName: DocumentsIcon,
                 id: 2,
                 modalId: 'documentsModal',
-                modalShow : false
+                modalShow: false
             }
         ]
     });
@@ -28,18 +28,29 @@ function DesktopIconContainer(){
     const setModalShowValue = (index, value) => {
         var iconsMeta = desktopIcons.icons;
         iconsMeta[index].modalShow = value;
-        setdesktopIcons({icons : iconsMeta});
+        setdesktopIcons({ icons: iconsMeta });
     }
 
+    const AppModalDetails = {
+        'thisPersonModal': {
+            topPanel: true,
+            sidePanel: true
+        },
 
+        'documentsModal': {
+            topPanel: true,
+            sidePanel: true
+        }
 
-    return(
+    };
+
+    return (
         <div className={`d-flex align-items-start flex-column bd-highlight mt-1`}>
-             {desktopIcons.icons.map((icon, index) =>{
-                 return(
+            {desktopIcons.icons.map((icon, index) => {
+                return (
                     <React.Fragment key={index}>
-                        <DesktopIconComponent                            
-                            onClickEvent={() => setModalShowValue(index,true)}
+                        <DesktopIconComponent
+                            onClickEvent={() => setModalShowValue(index, true)}
                             name={icon.name}
                             iconName={icon.iconName}
                         />
@@ -47,10 +58,12 @@ function DesktopIconContainer(){
                         <AppModal
                             data={icon.name}
                             show={icon.modalShow}
-                            onHide={() => setModalShowValue(index,false)}
+                            onHide={() => setModalShowValue(index, false)}
+                            modaldetails={AppModalDetails[icon.modalId]}
                         />
                     </React.Fragment>
-                 )}
+                )
+            }
             )}
         </div>
     );
