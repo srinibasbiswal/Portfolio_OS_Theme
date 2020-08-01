5d1d13ab719d07a027309787649b3bcb6ac3259e
Srinibas Biswal <srinibasbiswal12@gmail.com>
2020-08-01 16:52:44 +0530

    Changes for Modal top bar

--- src/components/AppModal.js
+++ src/components/AppModal.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import styles from '../stylesheets/style.module.css';
-import { Modal, Button, Figure } from 'react-bootstrap';
+import { Modal, Button, Image } from 'react-bootstrap';
 import CloseIcon from '../assets/images/CloseIcon.png';
 import MaximizeIcon from '../assets/images/MaximizeIcon.png';
 import MinimizeIcon from '../assets/images/MinimizeIcon.png';
+import DocumentIcon from '../assets/images/DocumentsIcon.png'
 
 function AppModal(props) {
 	return (
@@ -14,40 +15,52 @@ function AppModal(props) {
 			aria-labelledby="contained-modal-title-vcenter"
 			centered
 		>
-			<div className={`${styles.modalStaticTopBar}`}>
-				<div className={`d-flex flex-row-reverse bd-highlight`}>
-					<Figure className={`bd-highlight`} onClick={props.onHide}>
-						<Figure.Image
+			<div className={`${styles.AppExplorerStaticTop}`}>
+				<div class="d-flex bd-highlight">
+  					<div className="mr-auto bd-highlight">
+						<Image
 							width={25}
 							height={25}
-							alt="CloseIcon"
-							src={CloseIcon}
-						/>
-					</Figure>
-					<Figure className={`bd-highlight`}>
-						<Figure.Image
-							width={25}
-							height={25}
-							alt="MaximizeIcon"
-							src={MaximizeIcon}
-						/>
-					</Figure>
-					<Figure className={`bd-highlight`}>
-						<Figure.Image
+							alt="AppExplorerName"
+							src={DocumentIcon}
+							className={`m-0 p-0`}
+						></Image>
+						<span className={`font-weight-normal m-3`}>
+							App Explorer Title
+						</span>
+					</div>
+  					<div>
+					  	<Image className={styles.hoverPointer}
 							width={25}
 							height={25}
 							alt="MinimizeIcon"
 							src={MinimizeIcon}
-						/>
-					</Figure>
+						></Image>
+					</div>
+					<div>
+						<Image className={styles.hoverPointer}
+							width={25}
+							height={25}
+							alt="MaximizeIcon"
+							src={MaximizeIcon}
+						></Image>
+					</div>
+					<div>
+						<Image onClick={props.onHide} className={styles.hoverPointer}
+							width={25}
+							height={25}
+							alt="CloseIcon"
+							src={CloseIcon}
+						></Image>
+  					</div>
 				</div>
 			</div>
 			<Modal.Header>
-
 				<Modal.Title id="contained-modal-title-vcenter">
 					{props.data}
 				</Modal.Title>
 			</Modal.Header>
+
 			<Modal.Body>
 				<h4> {props.data}</h4>
 			</Modal.Body>
