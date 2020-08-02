4802cb46572441192af7e0e247cef5decd94206a
Srinibas Biswal <srinibasbiswal12@gmail.com>
2020-08-02 22:03:36 +0530

    Add min-height for data panel

--- src/components/AppExplorerDataPanel.js
+++ src/components/AppExplorerDataPanel.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from '../stylesheets/style.module.css';
 
 function AppExplorerDataPanel(props) {
     return (
-        <div>
+        <div className={`${styles.AppExplorerDataPanel}`}>
             {props.modaldetails.data}
         </div>
 
