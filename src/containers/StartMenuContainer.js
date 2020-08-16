7d67bd04d668e60fa76ee3289d0b4afa2a07bc17
Srinibas Biswal <srinibasbiswal12@gmail.com>
2020-08-16 20:20:48 +0530

    Modify Start menu 1st card

--- src/containers/StartMenuContainer.js
+++ src/containers/StartMenuContainer.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import styles from '../stylesheets/style.module.css';
-import { Card, Image, InputGroup, FormControl, Figure } from 'react-bootstrap';
+import { Card, Image, InputGroup, FormControl, Figure, Button } from 'react-bootstrap';
 import MailIcon from '../assets/images/MailIcon.png';
+import PowerOff from '../assets/images/PowerOff.png';
 import AboutMe from '../assets/images/AboutMeIcon.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; 
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
@@ -13,6 +14,7 @@ function StartMenuContainer(props){
                <Card className={` ${styles.ProfileCard} d-flex ${styles.BackgroundBlur} m-2`}>
                    <Image className={`img-circle rounded-circle mx-auto ${styles.CardImgTop}`} src={AboutMe}></Image>
                     <span className={`small text-center`}>{props.greetingMsg} Srinibas</span>
+                    <Image className={`${styles.CloseButton} img-circle rounded-circle`} src={PowerOff}></Image>
                     {/* <InputGroup className={`d-flex`}>                        
                         <FormControl
                         placeholder="Type here to search"
@@ -25,7 +27,7 @@ function StartMenuContainer(props){
                             </InputGroup.Text>
                         </InputGroup.Append>
                     </InputGroup> */}
-                    <div className={`d-flex justify-content-around shadow m-2`}>
+                    {/* <div className={`d-flex justify-content-around shadow m-2`}>
                         <Figure className={`border rounded ${styles.StartMenuFigure} text-center`}>
                             <Figure.Image
                                 width={50}
@@ -57,7 +59,10 @@ function StartMenuContainer(props){
                             </Figure.Caption>
                         </Figure>                    
 
-                    </div>
+                    </div> */}
+                    <p className={`text-center small`}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, <br/> sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    </p>
                </Card>
                <Card className = {`${styles.PinnedAppsCard} ${styles.BackgroundBlur} m-2`}>
 
