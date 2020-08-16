28f4a96fa136381bc68aa3b3288fce615ad86fd6
Srinibas Biswal <srinibasbiswal12@gmail.com>
2020-08-16 20:35:21 +0530

    Add Pinned Apps Card

--- src/containers/StartMenuContainer.js
+++ src/containers/StartMenuContainer.js
@@ -4,8 +4,11 @@ import { Card, Image, InputGroup, FormControl, Figure, Button } from 'react-boot
 import MailIcon from '../assets/images/MailIcon.png';
 import PowerOff from '../assets/images/PowerOff.png';
 import AboutMe from '../assets/images/AboutMeIcon.png';
+import CalendarIcon from '../assets/images/CalendarIcon.svg';
+import NotesIcon from '../assets/images/NotesIcon.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; 
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faThumbtack } from '@fortawesome/free-solid-svg-icons';
 
 function StartMenuContainer(props){
     return(
@@ -26,45 +29,48 @@ function StartMenuContainer(props){
                             <FontAwesomeIcon icon={faSearch}></FontAwesomeIcon> 
                             </InputGroup.Text>
                         </InputGroup.Append>
-                    </InputGroup> */}
-                    {/* <div className={`d-flex justify-content-around shadow m-2`}>
-                        <Figure className={`border rounded ${styles.StartMenuFigure} text-center`}>
-                            <Figure.Image
-                                width={50}
-                                src={MailIcon}
-                                className={`mb-0`}
-                            />
-                            <Figure.Caption>
-                                Mail
-                            </Figure.Caption>
-                        </Figure>
-                        <Figure className={`border rounded ${styles.StartMenuFigure} text-center`}>
-                            <Figure.Image
-                                width={50}
-                                src={MailIcon}
-                                className={`mb-0`}
-                            />
-                            <Figure.Caption>
-                                Mail
-                            </Figure.Caption>
-                        </Figure>
-                        <Figure className={`border rounded ${styles.StartMenuFigure} text-center`}>
-                            <Figure.Image
-                                width={50}
-                                src={MailIcon}
-                                className={`mb-0`}
-                            />
-                            <Figure.Caption>
-                                Mail
-                            </Figure.Caption>
-                        </Figure>                    
-
-                    </div> */}
+                    </InputGroup> */}                    
                     <p className={`text-center small`}>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, <br/> sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                     </p>
                </Card>
                <Card className = {`${styles.PinnedAppsCard} ${styles.BackgroundBlur} m-2`}>
+                   <div className={`m-2`}>
+                        <span class="badge badge-dark"><FontAwesomeIcon icon={faThumbtack}></FontAwesomeIcon> Pinned Apps </span>
+                   </div>
+                   <div className={`d-flex justify-content-around shadow m-2`}>
+                        <Figure className={`border rounded ${styles.StartMenuFigure} text-center`}>
+                            <Figure.Image
+                                width={50}
+                                src={MailIcon}
+                                className={`mb-0`}
+                            />
+                            <Figure.Caption className={styles.fontColorWhite}>
+                                Mail
+                            </Figure.Caption>
+                        </Figure>
+                        <Figure className={`border rounded ${styles.StartMenuFigure} text-center`}>
+                            <Figure.Image
+                                width={50}
+                                src={CalendarIcon}
+                                className={`mb-0`}
+                            />
+                            <Figure.Caption className={styles.fontColorWhite}>
+                                Calendar
+                            </Figure.Caption>
+                        </Figure>
+                        <Figure className={`border rounded ${styles.StartMenuFigure} text-center`}>
+                            <Figure.Image
+                                width={50}
+                                src={NotesIcon}
+                                className={`mb-0`}
+                            />
+                            <Figure.Caption className={styles.fontColorWhite}>
+                                Notes
+                            </Figure.Caption>
+                        </Figure>                    
+
+                    </div>
 
                </Card>
                <Card className = {`${styles.AllAppsCard} ${styles.BackgroundBlur} m-2`}>
