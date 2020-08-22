0080cdb4c79cfaff00dfebeb7f4a55ffba4bc5ad
Srinibas Biswal <srinibasbiswal12@gmail.com>
2020-08-23 02:21:37 +0530

    Add Calendar Component

--- src/components/ClockAndCalendar.js
+++ src/components/ClockAndCalendar.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card } from 'react-bootstrap';
+import InfiniteCalendar from 'react-infinite-calendar';
+import 'react-infinite-calendar/styles.css';
+import styles from '../stylesheets/style.module.css';
 
 function ClockAndCalendar(){
-    return(
 
+    return(
         <Card.Body>
-            hellofffffffffffffffffffffffffffffffffffffffffffffffffff
+            <Card className={`${styles.BackgroundBlur} ${styles.TaskBarPopupCard} m-2 d-flex`}>
+                <InfiniteCalendar
+                    width={400}
+                    height={300}
+                    selected={new Date()}
+                    className={`p-2 mx-auto`}
+                />
+            </Card>
         </Card.Body>
     );
 
