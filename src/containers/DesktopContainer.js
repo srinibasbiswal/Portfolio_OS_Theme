dbdecd37c034c8178151dcfc595aed706018c07b
Srinibas Biswal <srinibasbiswal12@gmail.com>
2020-08-24 00:18:24 +0530

    Add RightClick Menu

--- src/containers/DesktopContainer.js
+++ src/containers/DesktopContainer.js
@@ -3,15 +3,31 @@ import { Container } from 'react-bootstrap';
 import styles from '../stylesheets/style.module.css';
 import TaskbarContainer from './TaskbarContainer';
 import DesktopIconContainer from './DesktopIconContainer';
+import { ContextMenu, MenuItem, ContextMenuTrigger } from "react-contextmenu";
 
  function DesktopContainer(){
      return (
          <div>
-             <Container className ={styles.DesktopContainer}>
-                 <DesktopIconContainer></DesktopIconContainer>
-                 <TaskbarContainer></TaskbarContainer>
-             </Container>
-         </div>
+            <ContextMenuTrigger id="same_unique_identifier">
+                <Container className ={styles.DesktopContainer}>
+                    <DesktopIconContainer></DesktopIconContainer>
+                    <TaskbarContainer></TaskbarContainer>
+                </Container>
+            </ContextMenuTrigger>
+
+            <ContextMenu id="same_unique_identifier">
+                <MenuItem data={'th1'}>
+                ContextMenu Item 1
+                </MenuItem>
+                <MenuItem data={'th2'} >
+                ContextMenu Item 2
+                </MenuItem>
+                <MenuItem divider />
+                <MenuItem data={'th3'}>
+                ContextMenu Item 3
+                </MenuItem>
+            </ContextMenu>
+       </div>
      );
  }
 
