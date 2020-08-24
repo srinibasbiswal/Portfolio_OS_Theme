d4baa5a0be2af94d850fea210e0ef553677f658d
Srinibas Biswal <srinibasbiswal12@gmail.com>
2020-08-24 21:03:39 +0530

    Add initial Data Set

---
+++ src/components/dataComponents/dataSet.js
@@ -0,0 +1,12 @@
+import justAPic from '../../assets/images/justAPic.svg';
+
+export const Person = {
+    name: 'Srinibas Biswal',
+    profilePic : justAPic
+};
+
+export const AboutMeData = {
+    intro: ' I\'m a techno-functional Software Developement Engineer from India.',
+    desc : 'I enjoy taking complex problems and convertin to an usable format. My major domains are Web Devleopment and Python Prgramming.',
+    outro: 'When I\'m not coding, you will find me watching an episode of The Office or travelling to a new place.'
+};
