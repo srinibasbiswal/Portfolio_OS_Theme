74b96f261c5133202de682e1a090e95c8c5320e2
srinibasbiswal <srinibasbiswal12@gmail.com>
2021-05-22 02:13:36 +0530

    init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+	purge: [],
+	purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
