cb13f4dc52779bab1712026f72626fef3e466ada
srinibasbiswal <srinibasbiswal12@gmail.com>
2021-05-22 12:46:08 +0530

    define structure

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "uikit/dist/css/uikit.min.css";
+import "uikit/dist/css/uikit-core.min.css";
+import "uikit/dist/js/uikit.min.js";
+import "uikit/dist/js/uikit-icons.min";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
