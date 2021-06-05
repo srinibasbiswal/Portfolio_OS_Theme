968ec58363f67db9decba3f36b16510f1f450431
srinibasbiswal <srinibasbiswal12@gmail.com>
2021-06-05 17:08:57 +0530

    remove src

--- src/utils/Documents/system.document.js
+++
@@ -1,9 +0,0 @@
-class SystemStateDocument {
-	constructor() {
-		this.isLocked = true;
-		this.isShutDown = false;
-		this.isDesktop = false;
-	}
-}
-
-export default SystemStateDocument;
--- src/utils/actions/system.action.js
+++
@@ -1,8 +0,0 @@
-import { ACTION_TYPES } from "../documents/enums";
-
-export const setSystemState = (systemState) => {
-	return {
-		type: ACTION_TYPES.SET_STATE,
-		systemState: systemState,
-	};
-};
